c92d48553aaef3209a775af0feaf77b1cefddf27
seveshy <danielalvesmajor@outlook.com>
2020-10-24 10:33:05 -0300

    Connection database

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,20 @@ import express from 'express'
 
 const app = express()
 
+import './database/connection'
+
+// GET = BUSCAR UMA INFORMAÇÃO
+// POST = CRIAR UMA INFORMAÇÃO
+// PUT = EDITANDO UMA INFORMAÇÃO
+// DELETE = DELETANDO UMA INFORMAÇÃO
+
 app.get('/teste', (request, response) => {
+    return response.json({ message: 'Hello World'})
+})
+
+app.post('/teste', (request, response) => {
+    console.log(request.query)
+
     return response.json({ message: 'Hello World'})
 })
 
